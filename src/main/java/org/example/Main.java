19b355f4653f37ce1f199d5abb328f39c550f8b2
IsaacFS07 <121462046+IsaacFS07@users.noreply.github.com>
2026-02-27 16:16:05 +0100

    Add discount calculation and input handling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,41 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args){
 
+        int res, precioBase, descuento;
+        Scanner leer=new Scanner(System.in);
 
+        System.out.println("Dame el precio inicial");
+        precioBase=leer.nextInt();
+        System.out.println("Dame el descuento");
+        descuento=leer.nextInt();
+
+        try {
+             res = calcularPrecioFinal(precioBase, descuento);
+            System.out.println("El precio final es: "+ res);
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR FATAL: "+ e);
+        }
 
 
     }
 
     public static int calcularPrecioFinal(int precioBase, int descuento) {
+
+        if (precioBase < 0) {
+            throw new IllegalArgumentException("Precio invalido");
+        }
         if (descuento < 0 || descuento > 100) {
-            throw new IllegalArgumentException("Descuento inválido");
+            throw new IllegalArgumentException("Descuento invalido");
         }
         return precioBase - (precioBase * descuento / 100);
     }
+
+
     public static int maximo(int[] datos) {
         if (datos.length == 0) {
             throw new IllegalArgumentException("Array vacío");
